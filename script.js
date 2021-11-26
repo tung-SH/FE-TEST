65d1baf3e3ade9cc711f911cab86b7635d915fb6
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2021-11-26 10:31:29 +0700

    finish building the createItem function

--- script.js
+++ script.js
@@ -4,19 +4,23 @@
 
 // input
 const input = document.body.querySelector('input');
+const button = document.body.querySelector('button')
 
-input.addEventListener('input', (e) => {
-    createItem(e)
+// input.addEventListener('input', (e) => {
+//     createItem(e)
+// })
+button.addEventListener('click', () => {
+    createItem()
 })
 
 
 // output
 const list = document.body.querySelector('.list');
 
-function createItem(e) {
-    let li = document.createElement('li');
-    li.innerHTML = e.target.value;
-
-    list.append(li);
+function createItem() {
+    let div = document.createElement('div');
+    div.innerHTML = input.value;
+    // console.log(input.value);
+    list.append(div);
 }
 
