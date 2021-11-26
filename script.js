b92155004bd3701ef48a91589c04a6f647278ffc
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2021-11-26 10:16:52 +0700

    trying building createItem function for FE test

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// input 
+/****************************
+ *      CREATING FUNCTION
+ */
+
+// input
+const input = document.body.querySelector('input');
+
+input.addEventListener('input', (e) => {
+    createItem(e)
+})
+
+
+// output
+const list = document.body.querySelector('.list');
+
+function createItem(e) {
+    let li = document.createElement('li');
+    li.innerHTML = e.target.value;
+
+    list.append(li);
+}
+
