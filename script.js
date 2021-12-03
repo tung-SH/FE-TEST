a96dcbbecda396645f5352c846e61ec902bf9a94
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2021-12-03 19:37:17 +0700

    little change

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const button = document.body.querySelector('button')
 button.addEventListener('click', createItem)
 
 
-
 // output
 const list = document.body.querySelector('.list');
 const input = document.body.querySelector('input');
